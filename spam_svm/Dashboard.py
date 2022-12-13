5216125f010453c218b2cd57504114e123a5844e
ray-code2 <54462152+ray-code2@users.noreply.github.com>
2022-12-13 17:59:05 +0700

    Update Dashboard.py

--- spam_svm/Dashboard.py
+++ spam_svm/Dashboard.py
@@ -189,6 +189,8 @@ def ambil_komen(url, angka, semua):
 #     option.add_argument("--mute-audio")
 #     option.add_argument("start-maximized")
 #     option.add_argument('--headless')
+    options = Options()
+    options.add_argument('--disable-blink-features=AutomationControlled')
     service= Service('spam_svm/chromedriver')
     driver = webdriver.Remote(service.service_url)
     time.sleep(5)
