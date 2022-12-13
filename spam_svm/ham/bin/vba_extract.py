c4779faa2ebae04ce9bf86cc594bb13eb2bb3c1c
ray-code2 <54462152+ray-code2@users.noreply.github.com>
2022-12-13 17:46:00 +0700

    Delete spam_svm/ham directory

--- spam_svm/ham/bin/vba_extract.py
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:cf6d986dc4b666eb6b32117ea185f5b713c9bbe692eb3180e6de49bbef7caa4d
-size 1892
--- spam_svm/ham/lib/python3.10/site-packages/pydeck/widget/debounce.py
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:89a233221588b558706ad71756784671cddc88ee1b988042dfd87efd7b60e095
-size 694
