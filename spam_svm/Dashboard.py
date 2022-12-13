ad1a2ac139ec33eef2e115985231d66f3b544c56
ray-code2 <54462152+ray-code2@users.noreply.github.com>
2022-12-13 19:12:01 +0700

    Update Dashboard.py

--- spam_svm/Dashboard.py
+++ spam_svm/Dashboard.py
@@ -200,6 +200,7 @@ def ambil_komen(url, angka, semua):
     option.add_argument('--disable-blink-features=AutomationControlled')
 #     s=Service('spam_svm/chromedriver.exe')
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()),options=option)
+    service.start()
     driver.get(url)
     wait = WebDriverWait(driver,25)
     
