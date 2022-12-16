cfb360f3fc6ffb00d34c5f2a6d2616a83017c861
ray-code2 <54462152+ray-code2@users.noreply.github.com>
2022-12-16 19:14:05 +0700

    Update Dashboard.py

--- spam_svm/Dashboard.py
+++ spam_svm/Dashboard.py
@@ -216,7 +216,7 @@ def installff():
 def ambil_komen(url, angka, semua):
 #     chromedriver_autoinstaller.install()
     option = Options()
-    option.binary_location = r'/home/appuser/.wdm/drivers/geckodriver/linux64/0.32/geckodriver'
+    option.binary_location = r'/home/appuser/.wdm/drivers/geckodriver/linux64/0.32/geckodriver.exe'
 #     option.add_argument("--headless") #headless
     option.add_argument("--mute-audio")
     option.add_argument("--disable-gpu")
